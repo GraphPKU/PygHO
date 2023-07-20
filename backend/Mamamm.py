0db1314eed304cc6c6cebf2256aa9eeb46884592
wangxiyuan <wangxiyuan@pku.edu.cn>
2023-07-21 01:11:21 +0800

    mamamm

--- backend/Mamamm.py
+++ backend/Mamamm.py
@@ -1,4 +1,4 @@
-from .MaTensor import MaskedTensor
+from MaTensor import MaskedTensor
 import torch
 from torch import BoolTensor, Tensor
 from typing import Optional
@@ -8,16 +8,18 @@ def mamamm(A: MaskedTensor, B: MaskedTensor, mask: Optional[BoolTensor]=None):
     '''
     A: (B, n, m, *) MaskedTensor
     B: (B, m, k, *) MaskedTensor
+    * should be broadcastable
     '''
-    return MaskedTensor(torch.einsum("bnmd,bmkd->bnkd", A.get_data(), B.get_data()), mask if mask is not None else A.get_mask())
+    return MaskedTensor(torch.einsum("bnm...,bmk...->bnk...", A.fill_masked(0), B.fill_masked(0)), mask if mask is not None else A.mask)
 
 
 def mamm(A: MaskedTensor, B: Tensor, mask: Optional[BoolTensor]=None):
     '''
     A: (B, n, m, *) MaskedTensor
     B: (B, m, k, *) MaskedTensor
+    * should be broadcastable
     '''
-    return MaskedTensor(torch.einsum("bnmd,bmkd->bnkd", A.get_data(), B), mask if mask is not None else A.get_mask())
+    return MaskedTensor(torch.einsum("bnm...,bmk...->bnk...", A.fill_masked(0), B), mask if mask is not None else A.mask)
 
 
 def mmamm(A: Tensor, B: MaskedTensor, mask: Optional[BoolTensor]=None):
@@ -25,4 +27,17 @@ def mmamm(A: Tensor, B: MaskedTensor, mask: Optional[BoolTensor]=None):
     A: (B, n, m, *) MaskedTensor
     B: (B, m, k, *) MaskedTensor
     '''
-    return MaskedTensor(torch.einsum("bnmd,bmkd->bnkd", A, B.get_data()), mask if mask is not None else A.get_mask())
+    return MaskedTensor(torch.einsum("bnm...,bmk...->bnk...", A, B.fill_masked(0)), mask if mask is not None else B.mask)
+
+
+if __name__ == "__main__":
+    b, n, m, l, d = 3, 10, 20, 17, 13
+    A = torch.randn((b, n, m, d))
+    mask = torch.randn((b, n, l))>=1
+    MA = MaskedTensor(A, A.mean(dim=-1)>=0)
+    B = torch.randn((b, m, l, 1))
+    MB = MaskedTensor(B, B.mean(dim=-1)>=0)
+    MC = mamamm(MA, MB, mask)
+    MD = mamm(MA, B, mask)
+    ME = mmamm(A, MB, mask)
+    print(MC.shape, MD.shape, ME.shape)
